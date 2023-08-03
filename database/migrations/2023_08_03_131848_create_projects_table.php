475f401e545a91ab714a331cb4a77af534b87587
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-03 15:28:03 +0200

    [FEAT] eseguita migration tabelle

--- database/migrations/2023_08_03_131848_create_projects_table.php
+++ database/migrations/2023_08_03_131848_create_projects_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->string('link',50);
+            $table->string('slug',50);
             $table->timestamps();
         });
     }
