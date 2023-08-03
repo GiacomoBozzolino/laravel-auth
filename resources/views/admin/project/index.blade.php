5a18adfa52cf18e0eb17399eb1dbd2ab5d2c9d6d
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-03 16:59:31 +0200

    [FEAT] stampa a schermo contenuto tabella

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -11,28 +11,18 @@
                     <th scope="col">Name</th>
                     <th scope="col">Link</th>
                     <th scope="col">Slug</th>
-                    <th scope="col">Handle</th>
                   </tr>
                 </thead>
                 <tbody>
+                    @foreach ($projects as $project)
                   <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
+                    <td>{{$project-> name}}</td>
+                    <td>{{$project-> link}}</td>
+                    <td>{{$project-> slug}}</td>
+                        
+                </tr>
+                @endforeach
+                  
                 </tbody>
 
         </div>
