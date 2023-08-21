48d29cccae684d08cd8dc5b77abfc91ce754211f
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-21 17:44:11 +0200

    [FEAT] scrittura file js per modale

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,32 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+
+const deleteSubmitButton = document.querySelectorAll('.delete-project-form button[type="submit"]');
+
+deleteSubmitButton.forEach((button)=> {
+    event.preventDefault();
+
+    const modal =document.getElementById('ConfirmProjectDelete');
+
+    const project_name =button.getAttribute('data-project-name');
+
+
+    const bootstrapModal= new bootstrap.Modal(modal);
+    bootstrapModal.show();
+
+    const projectNameYield = modal.querySelector('#modal-project-name')
+
+    projectNameYield.textContent =project_name;
+
+
+
+    const ButtonDelete =document.querySelector('.confirm-delete-button');
+    ButtonDelete.addEventListener ('click',()=>{
+        button.parentElemetn.submit();
+    })
+
+
+
+})
