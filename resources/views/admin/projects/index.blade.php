1bf3605c36ffb858aaed2fad613a9698b5b83dc4
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-21 15:14:58 +0200

    [FEAT] creazione file show e settate rotte

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,31 +1,33 @@
 @extends('layouts.admin')
 
 @section('content')
-
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <table class="table table-striped">
-                <thead>
-                  <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Link</th>
-                    <th scope="col">Slug</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    @foreach ($projects as $project)
-                  <tr>
-                    <td>{{$project-> name}}</td>
-                    <td>{{$project-> link}}</td>
-                    <td>{{$project-> slug}}</td>    
+  <div class="container">
+      <div class="row">
+          <div class="col-12">
+              <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Link</th>
+                      <th scope="col">Slug</th>
                     </tr>
-                @endforeach
-                  
-                </tbody>
+                  </thead>
+                  <tbody>
+                    @foreach ($projects as $project)
+                      <tr>
+                        <td>{{$project-> name}}</td>
+                        <td>{{$project-> link}}</td>
+                        <td>{{$project-> slug}}</td>   
+                        <td><a href="{{route('admin.projects.show', $project->id)}}" class="btn btn-primary">
+                          <i class="fas fa-eye"></i>
+                        </a></td>  
+                       
+                      </tr>
 
-        </div>
-    </div>
-</div>
-
+                    @endforeach  
+                  </tbody>
+              </table>
+          </div>
+      </div>
+  </div>
 @endsection
---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="p-2 text-end">
+                <a href="{{route('admin.projects.index' )}}" class="btn btn-primary"> Tutti i progetti</a>
+            </div>
+        </div>
+        <div class="col-12">
+            <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Link</th>
+                    
+                  </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                      <td>{{$project-> name}}</td>
+                      <td>{{$project-> link}}</td> 
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
