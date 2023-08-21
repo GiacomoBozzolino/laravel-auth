8ea7510294855640c51abebeecf33313b5cd6761
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-21 15:14:58 +0200

    [FEAT]inizio lavoro metodo show

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,31 +1,28 @@
 @extends('layouts.admin')
 
 @section('content')
-
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <table class="table table-striped">
-                <thead>
-                  <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Link</th>
-                    <th scope="col">Slug</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    @foreach ($projects as $project)
-                  <tr>
-                    <td>{{$project-> name}}</td>
-                    <td>{{$project-> link}}</td>
-                    <td>{{$project-> slug}}</td>    
+  <div class="container">
+      <div class="row">
+          <div class="col-12">
+              <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Link</th>
+                      <th scope="col">Slug</th>
                     </tr>
-                @endforeach
-                  
-                </tbody>
-
-        </div>
-    </div>
-</div>
-
+                  </thead>
+                  <tbody>
+                    @foreach ($projects as $project)
+                      <tr>
+                        <td>{{$project-> name}}</td>
+                        <td>{{$project-> link}}</td>
+                        <td>{{$project-> slug}}</td>    
+                      </tr>
+                    @endforeach  
+                  </tbody>
+              </table>
+          </div>
+      </div>
+  </div>
 @endsection
